d26a4f764e7a7149f17ebe0f2238dbc535d707be
NotAnEvilWaffle <herobrinebudder149@gmail.com>
2024-07-14 00:36:38 -0500

    Added static Input class

---
+++ Input/Input.cpp
@@ -0,0 +1,12 @@
+#include "Input.h"
+
+void Input::SetWindow(GLFWwindow *window)
+{
+	this->window = window;
+}
+
+bool Input::IsKeyPressed(int keyCode)
+{
+	return (glfwGetKey(window, keyCode) == GLFW_PRESS);
+}
+
